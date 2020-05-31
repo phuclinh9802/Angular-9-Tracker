34f7874f037c68bd33b6bdab921b6ab5694ed9f3
Phuc Nguyen <anicedream2002@gmail.com>
2020-05-31 15:41:28 -0500

    Auto-generated commit

--- projectpractice/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace projectpractice.Models
-{
-    public class User
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
